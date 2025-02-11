abdf3a67abc83b50f9e55dac6d9ff77243a08877
Vinayaka-Voleti <vinayaka.vvk@gmail.com>
2025-02-11 23:52:54 +0530

    initial

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React,{useState} from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleOnclick = ()=>{
+    setCount(count+ 1);
+  }
+
+
+  return (
+    <div>
+      <button onClick={handleOnclick}>{`Count is ${count}`}</button>
+    </div>
+  )
+}
+
+export default App
